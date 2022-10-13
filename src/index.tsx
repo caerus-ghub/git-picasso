dd0fdc8c0ef6ee36c44f85225abb3b79c6faf4dc
caerus <caerus.mbox@gmail.com>
2022-10-13 22:21:07 +0300

    add - next.js

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import App from 'components/App';
-
-const $root = document.getElementById('root') as Element
-const root = createRoot($root);
-
-root.render(<App/>)
